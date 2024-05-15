9496ff157fa7236c081d07aeef0644adeb6b7a1c
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2024-05-15 11:35:43 +0100

    add emoji picker

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
-import { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import ImageViewer from './components/ImageViewer';
-import Button from './components/Button';
 import * as ImagePicker from 'expo-image-picker';
-import IconButton from './components/IconButton';
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import Button from './components/Button';
 import CircleButton from './components/CircleButton';
+import EmojiPicker from './components/EmojiPicker';
+import IconButton from './components/IconButton';
+import ImageViewer from './components/ImageViewer';
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
   const [showAppOptions, setShowAppOptions] = useState(false);
   const [selectedImage, setSelectedImage] = useState(null);
 
@@ -32,7 +34,11 @@ export default function App() {
   };
 
   const onAddSticker = () => {
-    // TODO
+    setIsModalVisible(true);
+  };
+
+  const onModalClose = () => {
+    setIsModalVisible(false);
   };
 
   const onSaveImageAsync = async () => {
@@ -72,6 +78,10 @@ export default function App() {
           />
         </View>
       )}
+      <EmojiPicker
+        isVisible={isModalVisible}
+        onClose={onModalClose}
+      ></EmojiPicker>
       <StatusBar style="auto" />
     </View>
   );
